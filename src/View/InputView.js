7f6f3644ce395c03d3f5f0f57078fcedc91f59f9
Seungmani <dkelcncjs@naver.com>
2023-11-12 19:28:32 +0900

    [fix] 메뉴 목록 받고 return 수정

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -9,7 +9,8 @@ const InputView = {
   },
   async readMenu() {
     const menus = await Console.readLineAsync("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-    return menus;
+    const menuList = menus.split(",");
+    return menuList;
   },
 };
 
