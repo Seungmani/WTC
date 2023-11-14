2c4b249c76685a516c6189711aa7a68316a6bea2
Seungmani <dkelcncjs@naver.com>
2023-11-14 22:45:16 +0900

    [fix] ErrorTest 문구 수정

--- __tests__/ErrorTest.js
+++ __tests__/ErrorTest.js
@@ -18,7 +18,7 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe("예외 테스트", () => {
+describe("예외 테스트 추가 버전", () => {
   test("날짜 예외 테스트", async () => {
     // given
     const INVALID_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
