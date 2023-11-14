8f78688e24e6ebd714e25024a5e03e217048ae92
Seungmani <dkelcncjs@naver.com>
2023-11-14 17:34:27 +0900

    [fix] 공백 및 띄어쓰 수정

--- src/Constants/Validate.js
+++ src/Constants/Validate.js
@@ -6,40 +6,51 @@ const Validate = {
 		if (!Number(date)) throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
 		return Number(date);
 	},
+
 	validateDateTrim(date) {
 		if (date.length !== date.trim().length) throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
 	},
+
 	validateDateRange(date) {
 		if (Setting.FIRST_DAY > date || date > Setting.LAST_DAY) {
 			throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
 		}
 	},
+
 	validateNotOrder(order) {
 		if (order[0] === Setting.NOT_INPUT_MENU) throw new Error('[ERROR] 메뉴를 입력해 주세요');
 	},
+
 	validateIsMenu(order) {
 		if(!MenuList[order]) throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
 	},
+
 	validateDuplicate(orderList, order) {
 		const arrayForCheckDuplicate = Object.keys(orderList);
 		if (arrayForCheckDuplicate.includes(order)) throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.')
 	},
+
 	validateOrderForm(order) {
 		if (order.length !== Setting.NORMAL_SPLIT_LENGTH) throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
 	},
+
 	validateOnlyDrink(allMenu, drink=0) {
 		if(allMenu.length === drink) throw new Error ('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.')
 	},
+
 	validateOderCountTrim(oderCount) {
 		if(oderCount.length !== oderCount.trim().length) throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
 	},
+
 	validateTotalCount(totalCount) {
 		if (totalCount > 20) throw new Error('[ERROR] 총 메뉴 주문 수는 20개를 초과할 수 없습니다. 다시 입력해 주세요.');
 	},
+
 	validateNumberBiggerZero(count) {
 		if (Number(count) !==0 && !Number(count)) throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
 		return Number(count);
 	},
+	
 	validateZero(count) {
 		if (count === 0) throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
 	}
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -6,27 +6,37 @@ const OutputView = {
 	printWelcome(){
 		Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
 	},
+
 	printEvent(date) {
     Console.print(`12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`);
   },
+
   printMenu(menuList) {
     Console.print("<주문 메뉴>");
     Object.entries(menuList).forEach((menu) => {
       Console.print(`${menu[0]} ${menu[1]}개`);
     });
   },
+
 	printPrice(price) {
 		Console.print("\n<할인 전 총주문 금액>");
 		Console.print(`${price.toLocaleString('ko-KR')}원`)
 	},
+
   printGiveawayMenu(given) {
 		Console.print("\n<증정 메뉴>");
     given ? Console.print("샴페인 1개") : Console.print('없음');
   },
+
 	printBenefitDetails(dateDiscount, weekdayDiscount, weekendDiscount, starDiscount, giveawayDiscount) {
 		Console.print("\n<혜택 내역>");
 
-		if (dateDiscount === 0 && weekdayDiscount === 0 && weekendDiscount === 0 && giveawayDiscount === 0) {
+		if (
+			dateDiscount === 0 && 
+			weekdayDiscount === 0 && 
+			weekendDiscount === 0 && 
+			giveawayDiscount === 0
+			) {
 			Console.print('없음');
 			return;
 		}
@@ -36,18 +46,22 @@ const OutputView = {
 		starDiscount > 0 ? Console.print(`특별 할인: -${starDiscount.toLocaleString('ko-KR')}원`) : 0;
 		giveawayDiscount > 0 ? Console.print(`증정 이벤트: -${giveawayDiscount.toLocaleString('ko-KR')}원`) : 0;
 	},
+
 	printTotalBenefitPrice(totalBenefitPrice) {
 		Console.print("\n<총혜택 금액>");
 		totalBenefitPrice ? Console.print(`-${totalBenefitPrice.toLocaleString('ko-KR')}원`) : Console.print(`${totalBenefitPrice.toLocaleString('ko-KR')}원`);
 	},
+
 	printAfterDiscountPrice(price) {
 		Console.print("\n<할인 후 예상 결제 금액>");
 		Console.print(`${price.toLocaleString('ko-KR')}원`)
 	},
+
 	printEventBadge(badge) {
 		Console.print("\n<12월 이벤트 배지>");
 		badge !== '' ? Console.print(badge) : Console.print('없음');
 	},
+	
 	printError(message) {
 		Console.print(message);
 	}
